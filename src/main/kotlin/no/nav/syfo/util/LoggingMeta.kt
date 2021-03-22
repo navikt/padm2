d5b38e496036171fc3838f44f62b4109c688933b
AudunSorheim <audun.sorheim@nav.no>
2021-03-22 12:58:31 +0100

    Logger nå med type

--- src/main/kotlin/no/nav/syfo/util/LoggingMeta.kt
+++ src/main/kotlin/no/nav/syfo/util/LoggingMeta.kt
@@ -1,5 +1,35 @@
 package no.nav.syfo.util
 
+enum class LogType {
+    INVALID_MESSAGE,
+    TEST_FNR_IN_PRODUCTION
+}
+
+fun createLogEntry(
+    type: LogType,
+    loggingMeta: LoggingMeta
+): Map<String, String?> {
+    return mapOf(
+        "type" to type.name,
+        "mottakId" to loggingMeta.mottakId,
+        "orgNr" to loggingMeta.orgNr,
+        "msgId" to loggingMeta.msgId
+    )
+}
+
+fun createLogEntry(
+    type: LogType,
+    loggingMeta: LoggingMeta,
+    vararg arguments: Pair<String, String>
+): Map<String, String?> {
+    return mapOf(
+        "type" to type.name,
+        "mottakId" to loggingMeta.mottakId,
+        "orgNr" to loggingMeta.orgNr,
+        "msgId" to loggingMeta.msgId
+    ).plus(arguments)
+}
+
 data class LoggingMeta(
     val mottakId: String,
     val orgNr: String?,
