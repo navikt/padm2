7ba885b3613bf9175af7c719c6c1932c3bb41a8e
Mathias Rørvik <mathiasror@gmail.com>
2021-09-30 12:55:14 +0200

    Create sanitation function for PdfModel with metric (#57)

--- src/main/kotlin/no/nav/syfo/model/PdfModel.kt
+++ src/main/kotlin/no/nav/syfo/model/PdfModel.kt
@@ -1,5 +1,10 @@
 package no.nav.syfo.model
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.syfo.log
+import no.nav.syfo.metrics.SANITIZE_INVALID_CHAR_COUNTER
+import no.nav.syfo.objectMapper
+
 data class PdfModel(
     val dialogmelding: Dialogmelding,
     val validationResult: ValidationResult,
@@ -8,3 +13,25 @@ data class PdfModel(
     val navnSignerendeLege: String,
     val antallVedlegg: Int,
 )
+
+const val BYTE_ORDER_MARK = '\uFEFF'
+const val INVALID_CHAR = '\uFFFE'
+const val NUL = 0.toChar()
+
+val illegalCharacters = listOf(BYTE_ORDER_MARK, INVALID_CHAR)
+
+fun PdfModel.sanitizeForPdfGen(): PdfModel {
+    val pdfModelJsonString = objectMapper.writeValueAsString(this)
+
+    val sanitizedJson = pdfModelJsonString.toCharArray().filter {
+        if (it in illegalCharacters || it < NUL) {
+            SANITIZE_INVALID_CHAR_COUNTER.inc()
+            log.warn("Illegal character in PdfModel: %x".format(it.code))
+            false
+        } else {
+            true
+        }
+    }.toString()
+
+    return objectMapper.readValue(sanitizedJson)
+}
