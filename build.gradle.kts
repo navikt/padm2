214adbe003995f8b0aaf8a4444160535b17f0a06
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-01-04 11:45:40 +0100

    Remove redundant buildscript dependencies (#87)
    These class dependecies are included in the list of dependencies and loading them in as a buildscript is therefore redundant.

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,17 +47,6 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
-buildscript {
-    dependencies {
-        classpath("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
-        classpath("org.glassfish.jaxb:jaxb-runtime:2.4.0-b180830.0438")
-        classpath("com.sun.activation:javax.activation:1.2.0")
-        classpath("com.sun.xml.ws:jaxws-tools:2.3.1") {
-            exclude(group = "com.sun.xml.ws", module = "policy")
-        }
-    }
-}
-
 val githubUser: String by project
 val githubPassword: String by project
 
