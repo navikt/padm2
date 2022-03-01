e0a7ec1f4583eb16b5d7f2f7602050c2c20237eb
geir-waagboe <geir.arne.waagbo@nav.no>
2022-03-01 09:55:59 +0100

    Do not send apprec for syfomock-dialogmelding

--- src/main/kotlin/no/nav/syfo/services/ApprecService.kt
+++ src/main/kotlin/no/nav/syfo/services/ApprecService.kt
@@ -3,9 +3,11 @@ package no.nav.syfo.services
 import no.nav.helse.apprecV1.XMLAppRec
 import no.nav.helse.apprecV1.XMLCV
 import no.nav.helse.eiFellesformat2.XMLEIFellesformat
+import no.nav.helse.msgHead.XMLMsgHead
 import no.nav.syfo.application.mq.MQSenderInterface
 import no.nav.syfo.apprec.ApprecStatus
 import no.nav.syfo.apprec.createApprec
+import no.nav.syfo.logger
 import no.nav.syfo.metrics.APPREC_COUNTER
 import no.nav.syfo.util.apprecMarshaller
 import no.nav.syfo.util.get
@@ -17,10 +19,19 @@ fun sendReceipt(
     apprecStatus: ApprecStatus,
     apprecErrors: List<XMLCV> = listOf()
 ) {
-    val apprec = createApprec(fellesformat, apprecStatus)
-    apprec.get<XMLAppRec>().error.addAll(apprecErrors)
-    mqSender.sendReceipt(
-        payload = apprecMarshaller.toString(apprec)
-    )
-    APPREC_COUNTER.inc()
+    val msgHead: XMLMsgHead = fellesformat.get()
+    val msgId = msgHead.msgInfo.msgId
+    if (isTestDialogmelding(msgId)) {
+        logger.info("Msgid $msgId is test-dialogmelding from syfomock - skipping sending of apprec")
+    } else {
+        val apprec = createApprec(fellesformat, apprecStatus)
+        apprec.get<XMLAppRec>().error.addAll(apprecErrors)
+        mqSender.sendReceipt(
+            payload = apprecMarshaller.toString(apprec)
+        )
+        APPREC_COUNTER.inc()
+    }
 }
+
+private fun isTestDialogmelding(msgId: String): Boolean =
+    msgId.startsWith("syfomock-")
