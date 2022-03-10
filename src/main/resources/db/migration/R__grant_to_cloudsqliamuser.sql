071ae4aa27a77c3c3b898e3de327982860a6f581
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-03-10 13:20:02 +0100

    Create db-user isfyo-analyse (#123)

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,11 +1,5 @@
-DO $$
-BEGIN
-  CREATE ROLE cloudsqliamuser WITH NOLOGIN;
-  EXCEPTION WHEN DUPLICATE_OBJECT THEN
-  RAISE NOTICE 'not creating role cloudsqliamuser -- it already exists';
-END
-$$;
 
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
 -- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
