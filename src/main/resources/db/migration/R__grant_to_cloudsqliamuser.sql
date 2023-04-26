f70e61f485c69fbe3ac8596efe6cc8dd59a0ee67
geir-waagboe <geir.arne.waagbo@nav.no>
2023-04-26 12:50:45 +0200

    Update alerts using PrometheusRule

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,5 +1,5 @@
 
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
 -- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
