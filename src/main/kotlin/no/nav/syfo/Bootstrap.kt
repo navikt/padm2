759577af4c40b1a4176c760aae9f531163b3e9df
geir-waagboe <geir.arne.waagbo@nav.no>
2023-07-10 14:07:33 +0200

    Bump dependencies and add missing config

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -65,7 +65,11 @@ fun main() {
     val server = embeddedServer(
         factory = Netty,
         environment = applicationEngineEnvironment,
-    )
+    ) {
+        connectionGroupSize = 8
+        workerGroupSize = 8
+        callGroupSize = 16
+    }
 
     Runtime.getRuntime().addShutdownHook(
         Thread {
