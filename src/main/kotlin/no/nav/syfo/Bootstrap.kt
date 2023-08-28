378a5accf6d5ae942d745951b14939e5b2f4f880
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2023-08-28 10:37:09 +0200

    Enable after switch to TLS (#205)

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -19,6 +19,7 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
+import javax.jms.Session
 
 val logger: Logger = LoggerFactory.getLogger("no.nav.syfo.padm2")
 
@@ -130,7 +131,6 @@ fun launchListeners(
         smtssClient = smtssClient,
         emottakService = emottakService,
     )
-/*
     launchBackgroundTask(
         applicationState = applicationState,
     ) {
@@ -151,7 +151,6 @@ fun launchListeners(
             blockingApplicationRunner.run()
         }
     }
-*/
     launchBackgroundTask(
         applicationState = applicationState,
     ) {
