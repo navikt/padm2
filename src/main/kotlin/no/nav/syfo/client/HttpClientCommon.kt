49f148831fbab3741de772d00cdf1e0c27e09ab7
geir-waagboe <geir.arne.waagbo@nav.no>
2024-03-15 09:35:43 +0100

    Increase socket timeout pdfgen

--- src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
+++ src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
@@ -27,6 +27,7 @@ val pdfGenConfig: HttpClientConfig<out HttpClientEngineConfig>.() -> Unit = {
     this.commonConfig()
     install(HttpTimeout) {
         requestTimeoutMillis = 120 * 1000
+        socketTimeoutMillis = 120 * 1000
     }
 }
 
